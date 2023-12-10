756a48d9d9f3b50ded8e3b2609c747d1b697121e
Daryl Blough <darylxcuf@gmail.com>
2023-12-10 18:32:31 +0000

    fixed apollo sandbox introspection error in authMiddleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,36 +1,35 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const secret = process.env.AUTH_SECRET;
+// const secret = process.env.AUTH_SECRET;
+const secret = 'laskjdlfasldjfkskldjfksald';
 const expiration = '2h';
 
 module.exports = {
-    authMiddleware: function ({req}) {
-        // allows token to be set by req.body, req.query or headers
-        let token = req.body.token || req.query.token || req.authorization.headers;
+    authMiddleware: function ({ req }) {
+        // allows token to be set by req.body, req.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
 
-        // req ["Bearer", <token value>]
-        if (req.authorization.headers) {
-            token = token.split('').pop().trim();
-        }
-
+        // Check if token is present and not null
         if (!token) {
-            return req;
+            return { req };
         }
 
         try {
-            const { data } = jwt.verify(token, secret, {maxAge: expiration});
+            // Extract the actual token from the Authorization header
+            token = token.split(' ')[1];
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
             req.user = data;
-        } catch {
-            console.log('invalid token')
+        } catch (error) {
+            console.log('Invalid token:', error.message);
         }
 
-        return req;
+        return { req };
     },
 
-    signToken: function ({username, email, _id}) {
-        const payload = {username, email, _id};
-
-        return jwt.sign({data: payload}, secret, {expiresIn: expiration})
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     }
-}
+};
+
