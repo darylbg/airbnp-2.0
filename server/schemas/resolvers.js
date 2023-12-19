076524d837205cebfd8cba92247e8162c35b7d56
Daryl Blough <darylxcuf@gmail.com>
2023-12-19 22:50:24 +0000

    added error handling to register resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -113,17 +113,33 @@ const resolvers = {
         console.log(error)
       }
     },
-    register: async (parent, {userData}) => {
+    register: async (parent, { userData }) => {
       try {
+        console.log('Received user data:', JSON.stringify(userData));
+    
+        const existingUserName = await User.findOne({ display_name: userData.display_name });
+        const existingEmail = await User.findOne({ email: userData.email });
+    
+        if (existingUserName) {
+          console.log('Username already in use');
+          throw new ApolloError('Username already in use', 'DUPLICATE_DISPLAY_ERROR');
+        } else if (existingEmail) {
+          console.log('Email already in use');
+          throw new ApolloError('Email already in use', 'DUPLICATE_EMAIL_ERROR');
+        }
+    
         const newUser = await User.create(userData);
         const token = signToken(newUser);
-        console.log(`new user token ${userData}`);
-        return {token, newUser};
-        
+    
+        console.log('New user data:', JSON.stringify(newUser));
+        return { token, newUser };
       } catch (error) {
-        console.log(error)
+        console.log(error);
+        // Return null or handle the error as needed
+        throw error;
       }
     }
+    
   }
 };
 
