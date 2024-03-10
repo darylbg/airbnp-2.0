7901268171b96c3ece7edcb7de0a3a8d02fbad6d
Daryl Blough <darylxcuf@gmail.com>
2024-03-10 15:45:46 +0000

    set up mobile navbar account submenu

--- client/src/components/MobileNavbar/index.jsx
+++ client/src/components/MobileNavbar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, forwardRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import * as Dialog from "@radix-ui/react-dialog";
 import { HomeIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
@@ -13,9 +13,10 @@ import "./MobileNavbar.css";
 const MobileNavbar = forwardRef((props, ref) => {
   const [toggleSignInRegister, setToggleSignInRegister] = useState(true);
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [toggleAccountSubmenu, setToggleAccountSubmenu] = useState(false);
 
-  const {isLoggedIn} = useSelector((state) => state.auth);
-  console.log(isLoggedIn)
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  console.log(isLoggedIn);
   const navigate = useNavigate();
   // set sign in form to default display when dialog is opened
   useEffect(() => {
@@ -38,106 +39,151 @@ const MobileNavbar = forwardRef((props, ref) => {
 
   return (
     <>
-    <NavigationMenu.Root className="mobile-navbar-root">
-      <NavigationMenu.List className="mobile-navbar-list">
-        <NavigationMenu.Item className="mobile-navbar-item">
-          <NavigationMenu.Link className="mobile-navbar-link" asChild>
-            <Link to="/">
-              <span className="material-symbols-outlined mobile-navbar-icon">
-                Home
-              </span>
-              <span className="mobile-navbar-text">Home</span>
-            </Link>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item className="mobile-navbar-item">
-          <NavigationMenu.Link className="mobile-navbar-link" asChild>
-            <Link to="/search">
-              {" "}
-              <span className="material-symbols-outlined mobile-navbar-icon">
-                Search
-              </span>
-              <span className="mobile-navbar-text">Search</span>
-            </Link>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item className="mobile-navbar-item">
-          <NavigationMenu.Link className="mobile-navbar-link" asChild>
-            <Link to="/about">
-              {" "}
-              <span className="material-symbols-outlined mobile-navbar-icon">
-                description
-              </span>
-              <span className="mobile-navbar-text">About</span>
-            </Link>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item className="mobile-navbar-item">
-          <NavigationMenu.Link className="mobile-navbar-link" asChild>
-            <Link to="/contact">
-              {" "}
-              <span className="material-symbols-outlined mobile-navbar-icon">
-                Call
-              </span>
-              <span className="mobile-navbar-text">Contact</span>
-            </Link>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        {isLoggedIn ? (
+      <NavigationMenu.Root className="mobile-navbar-root">
+        <NavigationMenu.List className="mobile-navbar-list">
           <NavigationMenu.Item className="mobile-navbar-item">
-          <NavigationMenu.Link className="mobile-navbar-link" asChild>
-            <Link to="/account">
-              {" "}
-              <span className="material-symbols-outlined mobile-navbar-icon">
-                account_circle
-              </span>
-              <span className="mobile-navbar-text">Account</span>
-            </Link>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        ) : (
-          <NavigationMenu.Item className="mobile-navbar-item">
-          <NavigationMenu.Link className="mobile-navbar-link" asChild>
-            <Dialog.Root
-              open={dialogOpen}
-              onOpenChange={() => setDialogOpen(!dialogOpen)}
-              ref={ref}
-            >
-              <Dialog.Trigger className="sign-in-button mobile-signIn-button">
+            <NavigationMenu.Link className="mobile-navbar-link" asChild>
+              <Link to="/">
                 <span className="material-symbols-outlined mobile-navbar-icon">
-                  account_circle
+                  Home
                 </span>
-                <span className="mobile-navbar-text">Sign in</span>
-              </Dialog.Trigger>
-              <Dialog.Portal>
-                <Dialog.Overlay className="signIn-dialog-overlay" />
-                <Dialog.Content className="signIn-dialog-content">
-                  {toggleSignInRegister ? (
-                    <SignInForm
-                      handleSignInRegisterToggle={handleSignInRegisterToggle}
-                    />
-                  ) : (
-                    <RegisterForm
-                      handleSignInRegisterToggle={handleSignInRegisterToggle}
-                    />
-                  )}
-                  <Dialog.Close asChild>
-                    <span className="material-symbols-outlined signIn-dialog-close">
-                      close
+                <span className="mobile-navbar-text">Home</span>
+              </Link>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item className="mobile-navbar-item">
+            <NavigationMenu.Link className="mobile-navbar-link" asChild>
+              <Link to="/search">
+                {" "}
+                <span className="material-symbols-outlined mobile-navbar-icon">
+                  Search
+                </span>
+                <span className="mobile-navbar-text">Search</span>
+              </Link>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item className="mobile-navbar-item">
+            <NavigationMenu.Link className="mobile-navbar-link" asChild>
+              <Link to="/about">
+                {" "}
+                <span className="material-symbols-outlined mobile-navbar-icon">
+                  description
+                </span>
+                <span className="mobile-navbar-text">About</span>
+              </Link>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item className="mobile-navbar-item">
+            <NavigationMenu.Link className="mobile-navbar-link" asChild>
+              <Link to="/contact">
+                {" "}
+                <span className="material-symbols-outlined mobile-navbar-icon">
+                  Call
+                </span>
+                <span className="mobile-navbar-text">Contact</span>
+              </Link>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+          {isLoggedIn ? (
+            <NavigationMenu.Item className="mobile-navbar-item">
+              <NavigationMenu.Link className="mobile-navbar-link" asChild>
+                {/* trigger submenu open/close */}
+                <Link onClick={() => setToggleAccountSubmenu(!toggleAccountSubmenu)}>
+                  {" "}
+                  <span className="material-symbols-outlined mobile-navbar-icon">
+                    account_circle
+                  </span>
+                  <span className="mobile-navbar-text">Account</span>
+                </Link>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+          ) : (
+            <NavigationMenu.Item className="mobile-navbar-item">
+              <NavigationMenu.Link className="mobile-navbar-link" asChild>
+                <Dialog.Root
+                  open={dialogOpen}
+                  onOpenChange={() => setDialogOpen(!dialogOpen)}
+                  ref={ref}
+                >
+                  <Dialog.Trigger className="sign-in-button mobile-signIn-button">
+                    <span className="material-symbols-outlined mobile-navbar-icon">
+                      account_circle
                     </span>
-                  </Dialog.Close>
-                </Dialog.Content>
-              </Dialog.Portal>
-            </Dialog.Root>
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        )}
-      </NavigationMenu.List>
-    </NavigationMenu.Root>
+                    <span className="mobile-navbar-text">Sign in</span>
+                  </Dialog.Trigger>
+                  <Dialog.Portal>
+                    <Dialog.Overlay className="signIn-dialog-overlay" />
+                    <Dialog.Content className="signIn-dialog-content">
+                      {toggleSignInRegister ? (
+                        <SignInForm
+                          handleSignInRegisterToggle={
+                            handleSignInRegisterToggle
+                          }
+                        />
+                      ) : (
+                        <RegisterForm
+                          handleSignInRegisterToggle={
+                            handleSignInRegisterToggle
+                          }
+                        />
+                      )}
+                      <Dialog.Close asChild>
+                        <span className="material-symbols-outlined signIn-dialog-close">
+                          close
+                        </span>
+                      </Dialog.Close>
+                    </Dialog.Content>
+                  </Dialog.Portal>
+                </Dialog.Root>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+          )}
+        </NavigationMenu.List>
+        {/* mobile navbar account submenu popup */}
+        {toggleAccountSubmenu? (
+          <NavigationMenu.Sub className="mobile-account-submenu">
+          <NavigationMenu.List>
+            <NavigationMenu.Item className="mobile-navbar-item">
+              <NavigationMenu.Link className="mobile-navbar-link" asChild>
+                <Link to="/account" onClick={() => setToggleAccountSubmenu(!toggleAccountSubmenu)}>
+                  {" "}
+                  <span className="material-symbols-outlined mobile-navbar-icon">
+                    account_circle
+                  </span>
+                  <span className="mobile-navbar-text">Profile</span>
+                </Link>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item className="mobile-navbar-item">
+              <NavigationMenu.Link className="mobile-navbar-link" asChild>
+                <Link to="/dashboard" onClick={(() => setToggleAccountSubmenu(!toggleAccountSubmenu))}>
+                  {" "}
+                  <span className="material-symbols-outlined mobile-navbar-icon">
+                    <span class="material-symbols-outlined">
+                      dashboard_customize
+                    </span>
+                  </span>
+                  <span className="mobile-navbar-text">Dashboard</span>
+                </Link>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item className="mobile-navbar-item">
+              <NavigationMenu.Link className="mobile-navbar-link" asChild>
+                <Link onClick={logout}>
+                  {" "}
+                  <span className="material-symbols-outlined mobile-navbar-icon">
+                    logout
+                  </span>
+                  <span className="mobile-navbar-text">Logout</span>
+                </Link>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+          </NavigationMenu.List>
+        </NavigationMenu.Sub>
+        ) : null}
+      </NavigationMenu.Root>
     </>
   );
 });
 
 export default MobileNavbar;
-
-
