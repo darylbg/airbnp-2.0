1ca75bb87f59dc524155a0798d8f5e6906eb7afc
Daryl Blough <darylxcuf@gmail.com>
2024-03-12 19:34:39 +0000

    built mobile dashboard menu

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -10,61 +10,93 @@ import "./Dashboard.css";
 export default function Dashboard() {
   const currentUser = useSelector((state) => state.auth);
   const [activeDashboardTab, setActiveDashboardTab] = useState(0);
+  const [mobileDashboardMenu, setMobileDashboardMenu] = useState(false);
+
+  const handleActiveTab = (e) => {
+    e.preventDefault();
+    
+  };
+
+  const toggleMobileDashboardMenu = (e) => {
+    e.preventDefault();
+    setMobileDashboardMenu(!mobileDashboardMenu);
+  };
 
   return (
     <>
       <div className="dashboard">
         {currentUser.isLoggedIn ? (
           <>
-            <div className="dashboard-menu">
+            <div
+              className={`dashboard-menu ${
+                mobileDashboardMenu
+                  ? "mobile-dashboard-menu-open"
+                  : "mobile-dashboard-menu-closed"
+              }`}
+            >
               <div className="dashboard-menu-title hide-on-sm">
                 <h2>Dashboard</h2>
               </div>
               <div className="dashboard-menu-items">
                 <div className="dashboard-menu-item">
-                  <button onClick={() => setActiveDashboardTab(0)}
-                  className={activeDashboardTab === 0 ? "active-dashboard-tab" : ""}
+                  <button
+                    onClick={() => setActiveDashboardTab(0)}
+                    className={
+                      activeDashboardTab === 0 ? "active-dashboard-tab" : ""
+                    }
                   >
                     <span className="material-symbols-outlined">today</span>
-                    <span className="hide-on-sm text">Bookings</span>
+                    <span className="text">Bookings</span>
                   </button>
                 </div>
                 <div className="dashboard-menu-item">
-                <button onClick={() => setActiveDashboardTab(1)}
-                  className={activeDashboardTab === 1 ? "active-dashboard-tab" : ""}
+                  <button
+                    onClick={() => setActiveDashboardTab(1)}
+                    className={
+                      activeDashboardTab === 1 ? "active-dashboard-tab" : ""
+                    }
                   >
                     <span className="material-symbols-outlined">list</span>
-                    <span className="hide-on-sm text">Listings</span>
+                    <span className="text">Listings</span>
                   </button>
                 </div>
                 <div className="dashboard-menu-item">
-                <button onClick={() => setActiveDashboardTab(2)}
-                  className={activeDashboardTab === 2 ? "active-dashboard-tab" : ""}
+                  <button
+                    onClick={() => setActiveDashboardTab(2)}
+                    className={
+                      activeDashboardTab === 2 ? "active-dashboard-tab" : ""
+                    }
                   >
                     <span className="material-symbols-outlined">
                       Notifications
                     </span>
-                    <span className="hide-on-sm text">Notifications</span>
+                    <span className="text">Notifications</span>
                   </button>
                 </div>
                 <div className="dashboard-menu-item">
-                <button onClick={() => setActiveDashboardTab(3)}
-                  className={activeDashboardTab === 3 ? "active-dashboard-tab" : ""}
+                  <button
+                    onClick={() => setActiveDashboardTab(3)}
+                    className={
+                      activeDashboardTab === 3 ? "active-dashboard-tab" : ""
+                    }
                   >
                     <span className="material-symbols-outlined">payments</span>
-                    <span className="hide-on-sm text">Payments</span>
+                    <span className="text">Payments</span>
                   </button>
                 </div>
                 <div className="dashboard-menu-item">
-                <button onClick={() => setActiveDashboardTab(4)}
-                  className={activeDashboardTab === 4 ? "active-dashboard-tab" : ""}
+                  <button
+                    onClick={() => setActiveDashboardTab(4)}
+                    className={
+                      activeDashboardTab === 4 ? "active-dashboard-tab" : ""
+                    }
                   >
                     <span className="material-symbols-outlined">reviews</span>
-                    <span className="hide-on-sm text">Reviews</span>
+                    <span className="text">Reviews</span>
                   </button>
                 </div>
               </div>
-              <div className="dashboard-menu-footer hide-on-sm">
+              <div className="dashboard-menu-footer">
                 <img src={currentUser.user.user_image} alt="user avatar" />
                 <h3>
                   {currentUser.user.first_name +
@@ -74,11 +106,24 @@ export default function Dashboard() {
                 <p>{currentUser.user.email}</p>
               </div>
             </div>
+            <div className="mobile-dashboard-menu hide-on-md">
+              <button
+                onClick={toggleMobileDashboardMenu}
+              >
+                <span className="material-symbols-outlined">menu</span>
+              </button>
+              <div className="menu-header">
+                <h3>Dashboard</h3>
+              </div>
+              <div className="menu-avatar">
+                <img src={currentUser.user.user_image} alt="user avatar" />
+              </div>
+            </div>
             <div className="dashboard-content">
               {activeDashboardTab === 0 ? <Bookings /> : null}
-              {activeDashboardTab === 1 ? <Listings /> : null} 
+              {activeDashboardTab === 1 ? <Listings /> : null}
               {activeDashboardTab === 2 ? <Notifications /> : null}
-              {activeDashboardTab === 3 ? <Payments /> : null} 
+              {activeDashboardTab === 3 ? <Payments /> : null}
               {activeDashboardTab === 4 ? <Reviews /> : null}
             </div>
           </>
