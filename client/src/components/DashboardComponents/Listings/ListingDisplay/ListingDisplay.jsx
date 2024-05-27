c65c5fd6180889d726f4fceb594dc324ff1e189c
daryl <darylxcuf@gmail.com>
2024-05-27 20:08:55 +0100

    styled user listing display

--- client/src/components/DashboardComponents/Listings/ListingDisplay/ListingDisplay.jsx
+++ client/src/components/DashboardComponents/Listings/ListingDisplay/ListingDisplay.jsx
@@ -2,9 +2,27 @@ import React from "react";
 
 export default function ListingDisplay({ props }) {
   return (
-    <div>
-      listing with id:
-      {props.id}
+    <div className="listing">
+      <div className="listing-header">
+        <div className="listing-header-text"></div>
+        <div className="listing-header-action"></div>
+      </div>
+      <div className="listing-body">
+        <div className="listing-body-image">
+          <img src="" alt="" />
+        </div>
+        <div className="listing-body-content">
+          <div className="listing-title"></div>
+          <div className="listing-description"></div>
+          <div className="listing-amenities">
+            <div className="listing-amenity"></div>
+          </div>
+        </div>
+        <div className="listing-body-actions">
+          <div className="listing-button-group"></div>
+          <div className="listing-overview"></div>
+        </div>
+      </div>
     </div>
   );
 }
