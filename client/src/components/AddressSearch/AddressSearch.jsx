d1dff85875611d467268b5b0a6678f4b95511718
Daryl Blough <darylxcuf@gmail.com>
2024-06-25 22:05:50 +0100

    styled address search

--- client/src/components/AddressSearch/AddressSearch.jsx
+++ client/src/components/AddressSearch/AddressSearch.jsx
@@ -1,24 +1,32 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { AddressAutofill, AddressMinimap, useConfirmAddress, config } from '@mapbox/search-js-react';
+import React, { useState, useCallback, useEffect } from "react";
+import {
+  AddressAutofill,
+  AddressMinimap,
+  useConfirmAddress,
+  config,
+} from "@mapbox/search-js-react";
+
+import "./AddressSearch.css";
 
 export default function AddressSearch() {
   const [showFormExpanded, setShowFormExpanded] = useState(false);
   const [showMinimap, setShowMinimap] = useState(false);
   const [feature, setFeature] = useState();
   const [showValidationText, setShowValidationText] = useState(false);
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    const accessToken = 'MAPBOX_ACCESS_TOKEN';
-    setToken(accessToken)
+    const accessToken =
+      "pk.eyJ1IjoiZGF6emExMjMiLCJhIjoiY2x4cWZ0NmR0MHcwbTJpc2h0NGtlaDljZiJ9.Ci-aOXRO2xgWgSwfLY2Gtw";
+    setToken(accessToken);
     config.accessToken = accessToken;
-  }, [])
-  
+  }, []);
+
   const { formRef, showConfirm } = useConfirmAddress({
     minimap: true,
     skipConfirmModal: (feature) => {
-      ['exact', 'high'].includes(feature.properties.match_code.confidence)
-    }
+      ["exact", "high"].includes(feature.properties.match_code.confidence);
+    },
   });
 
   const handleRetrieve = useCallback(
@@ -32,92 +40,97 @@ export default function AddressSearch() {
   );
 
   function handleSaveMarkerLocation(coordinate) {
-    console.log(`Marker moved to ${JSON.stringify(coordinate)}.`)
+    console.log(`Marker moved to ${JSON.stringify(coordinate)}.`);
   }
 
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault();
-    const result = await showConfirm();
-    if (result.type === 'nochange') submitForm();
-  }, [showConfirm]);
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const result = await showConfirm();
+      if (result.type === "nochange") submitForm();
+    },
+    [showConfirm]
+  );
 
-  function submitForm() {
+  const submitForm = () => {
     setShowValidationText(true);
     setTimeout(() => {
       resetForm();
     }, 2500);
-  }
+  };
 
-  function resetForm() {
+  const resetForm = () => {
     const inputs = document.querySelectorAll("input");
-    inputs.forEach(input => input.value = "");
+    inputs.forEach((input) => (input.value = ""));
     setShowFormExpanded(false);
     setShowValidationText(false);
     setFeature(null);
-  }
+  };
 
   return (
     <>
-      <form ref={formRef} className="flex flex--column" onSubmit={handleSubmit}>
-        <div className="grid grid--gut24 mb60">
-          <div className="col col--auto-mm w-full">
-
+      <form ref={formRef} className="">
+        <div className="">
+          <div className="">
             {/* Input form */}
-            <label className="txt-s txt-bold color-gray mb3">Address</label>
+            
             <AddressAutofill accessToken={token} onRetrieve={handleRetrieve}>
+            <label className="">Address</label>
               <input
-                className="input mb12"
+                className=""
                 placeholder="Start typing your address, e.g. 123 Main..."
                 autoComplete="address-line1"
                 id="mapbox-autofill"
               />
             </AddressAutofill>
-            { !showFormExpanded && 
+            {!showFormExpanded && (
               <div
                 id="manual-entry"
-                className="w180 mt6 link txt-ms border-b color-gray color-black-on-hover"
+                className=""
                 onClick={() => setShowFormExpanded(true)}
               >
                 Enter an address manually
               </div>
-            }
-            <div className="secondary-inputs" style={{ display: showFormExpanded ? 'block' : 'none' }}>
-              <label className="txt-s txt-bold color-gray mb3">Address Line 2</label>
+            )}
+            <div
+              className=""
+              style={{ display: showFormExpanded ? "block" : "none" }}
+            >
+              <label className="">
+                Address Line 2
+              </label>
               <input
-                className="input mb12"
+                className=""
                 placeholder="Apartment, suite, unit, building, floor, etc."
                 autoComplete="address-line2"
               />
               <label className="txt-s txt-bold color-gray mb3">City</label>
               <input
-                className="input mb12"
+                className=""
                 placeholder="City"
                 autoComplete="address-level2"
               />
-              <label className="txt-s txt-bold color-gray mb3"
-                >State / Region</label
-              >
+              <label className="txt-s txt-bold color-gray mb3">
+                State / Region
+              </label>
               <input
-                className="input mb12"
+                className=""
                 placeholder="State / Region"
                 autoComplete="address-level1"
               />
-              <label className="txt-s txt-bold color-gray mb3"
-                >ZIP / Postcode</label
-              >
+              <label className="txt-s txt-bold color-gray mb3">
+                ZIP / Postcode
+              </label>
               <input
-                className="input"
+                className=""
                 placeholder="ZIP / Postcode"
                 autoComplete="postal-code"
               />
             </div>
           </div>
-          <div className="col col--auto-mm">
+          <div className="address-search-minimap">
             {/* Visual confirmation map */}
-            <div
-              id="minimap-container"
-              className="h240 w360 relative mt18"
-            >
+            <div id="minimap-container" className="h240 w360 relative mt18">
               <AddressMinimap
                 canAdjustMarker={true}
                 satelliteToggle={true}
@@ -130,23 +143,29 @@ export default function AddressSearch() {
         </div>
 
         {/* Form buttons */}
-        { showFormExpanded && 
-          <div className="mb30 submit-btns">
-            <button type="submit" className="btn round" id="btn-confirm">
+        {showFormExpanded && (
+          <div className="">
+            {/* <button type="submit" className="btn round" id="btn-confirm">
               Confirm
-            </button>
-            <button type="button" className="btn round btn--gray-light ml3" id="btn-reset" onClick={resetForm}>
-              Reset
+            </button> */}
+            <button
+              type="button"
+              className=""
+              id="btn-reset"
+              onClick={resetForm}
+            >
+              Clear
             </button>
           </div>
-        }
+        )}
       </form>
 
-
-      {/* Validation text */}
-      { showValidationText && 
-        <div id="validation-msg" className="mt24 txt-m txt-bold">Order successfully submitted.</div>
-      }
+      {/* Validation text
+      {showValidationText && (
+        <div id="validation-msg" className="">
+          Order successfully submitted.
+        </div>
+      )} */}
     </>
-  )
+  );
 }
