520cda29939c871d6d5c7a3f071460cd8311e8da
daryl <darylxcuf@gmail.com>
2024-06-29 13:02:37 +0100

    seperated personal info edit and password change into their respective account tiles

--- client/src/components/AccountSubComponents/LoginAndSecurity/LoginAndSecurity.jsx
+++ client/src/components/AccountSubComponents/LoginAndSecurity/LoginAndSecurity.jsx
@@ -1,7 +1,173 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as Form from "@radix-ui/react-form";
+import ToastComponent from "../../PrimitiveComponents/ToastComponent/ToastComponent";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import PrimaryButton from "../../PrimitiveComponents/PrimaryButton/PrimaryButton";
+import PasswordVisibilityToggle from "../../PrimitiveComponents/PasswordVisibilityToggle/PasswordVisibilityToggle";
+import Spinner from "../../PrimitiveComponents/Spinner/Spinner";
+import "../PersonalInfo/PersonalInfo.css";
 
 export default function LoginAndSecurity() {
+  const currentUser = useSelector((state) => state.userDetails.byId);
+  const dispatch = useDispatch();
+  // console.log(currentUser.user_image)
+  const [loading, setLoading] = useState(false);
+  const [saving, setSaving] = useState(false);
+  const [passwordEditable, setPasswordEditable] = useState(false);
+  
+  // change password logic
+  const hiddenPasswordPlaceholder = "******";
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    clearErrors,
+    formState: { errors: errorsPassword },
+  } = useForm({
+    defaultValues: {
+      currentPassword: "",
+      newPassword: "",
+      newPasswordConfirm: "",
+    },
+  });
+
+  const handlePasswordEditEnable = () => {
+    setPasswordEditable(true);
+  };
+
+  const cancelPasswordEdit = () => {
+    setValue("currentPassword", "");
+    setValue("newPassword", "");
+    setValue("newPasswordConfirm", "");
+
+    clearErrors("currentPassword");
+    clearErrors("newPassword");
+    clearErrors("newPasswordConfirm");
+
+    setPasswordEditable(false);
+  };
+
+  const handlePasswordChange = async (FormData) => {
+    setLoading(true);
+    setSaving(true);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      setValue("currentPassword", "");
+      setValue("newPassword", "");
+      setValue("newPasswordConfirm", "");
+
+      clearErrors("currentPassword");
+      clearErrors("newPassword");
+      clearErrors("newPasswordConfirm");
+
+      setPasswordEditable(false);
+      console.log("changed password", FormData);
+      toast.success(
+        <ToastComponent message="Successfully changed password." />
+      );
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+      setSaving(false);
+    }
+    console.log("successfully changed password", FormData);
+  };
+
   return (
-    <div>login and security</div>
-  )
+    <section className="personal-info">
+      {/* change password form */}
+      <div className="change-password">
+        <div className="change-password-enable">
+          <h3>Set a new password</h3>
+          {!passwordEditable ? (
+            <PrimaryButton
+              action={handlePasswordEditEnable}
+              loading={loading}
+              className="edit-button"
+            >
+              Change
+            </PrimaryButton>
+          ) : (
+            <PrimaryButton action={cancelPasswordEdit} loading={loading}>
+              Cancel
+            </PrimaryButton>
+          )}
+        </div>
+        <Form.Root
+          className="change-password-form"
+          onSubmit={handleSubmit(handlePasswordChange)}
+        >
+          <Form.Field className="edit-personal-info-field">
+            <Form.Label className="field-label">{passwordEditable ? "Enter your current password" : "Current password"}</Form.Label>
+            <Form.Control className="change-password-field" asChild>
+              <input
+                placeholder={hiddenPasswordPlaceholder}
+                disabled={!passwordEditable}
+                type="password"
+                {...register("currentPassword", {
+                  required: "This is required",
+                })}
+              />
+            </Form.Control>
+            <div className="field-message">
+              {errorsPassword.currentPassword?.message}
+            </div>
+          </Form.Field>
+          {passwordEditable && (
+            <>
+              <Form.Field className="edit-personal-info-field">
+                <Form.Label className="field-label">New password</Form.Label>
+                <div className="password-visibility-wrapper">
+                  <Form.Control asChild>
+                    <input
+                      type="password"
+                      {...register("newPassword", {
+                        required: "This is required",
+                      })}
+                    />
+                  </Form.Control>
+                  <PasswordVisibilityToggle action={(e) => {}} />
+                </div>
+                <div className="field-message">
+                  {errorsPassword.newPassword?.message}
+                </div>
+              </Form.Field>
+              <Form.Field className="edit-personal-info-field">
+                <Form.Label className="field-label">
+                  Retype new password
+                </Form.Label>
+                <div className="password-visibility-wrapper">
+                  <Form.Control asChild>
+                    <input
+                      type="password"
+                      {...register("newPasswordConfirm", {
+                        required: "This is required",
+                      })}
+                    />
+                  </Form.Control>
+                  <PasswordVisibilityToggle action={(e) => {}} />
+                </div>
+                <div className="field-message">
+                  {errorsPassword.newPasswordConfirm?.message}
+                </div>
+              </Form.Field>
+              <Form.Field className="edit-personal-info-field">
+                <Form.Submit asChild>
+                  <PrimaryButton className="save-button" loading={loading}>
+                    Save
+                    {saving? <Spinner /> : null}
+                  </PrimaryButton>
+                </Form.Submit>
+              </Form.Field>
+            </>
+          )}
+        </Form.Root>
+      </div>
+    </section>
+  );
 }
--- client/src/components/AccountSubComponents/PersonalInfo/PersonalInfo.jsx
+++ client/src/components/AccountSubComponents/PersonalInfo/PersonalInfo.jsx
@@ -8,7 +8,6 @@ import { UPDATE_USER_MUTATION } from "../../../utils/mutations/userMutations";
 import { useDispatch, useSelector } from "react-redux";
 import { updateUserDetails } from "../../../reducers/userDetailsReducer";
 import PrimaryButton from "../../PrimitiveComponents/PrimaryButton/PrimaryButton";
-import PasswordVisibilityToggle from "../../PrimitiveComponents/PasswordVisibilityToggle/PasswordVisibilityToggle";
 import Spinner from "../../PrimitiveComponents/Spinner/Spinner";
 import "./PersonalInfo.css";
 
@@ -18,15 +17,13 @@ export default function PersonalInfo() {
   // console.log(currentUser.user_image)
   const [loading, setLoading] = useState(false);
   const [personalInfoEditSaving, setPersonalInfoEditSaving] = useState(false);
-  const [passwordChangeSaving, setPasswordChangeSaving] = useState(false);
   const [editable, setEditable] = useState(false);
-  const [passwordEditable, setPasswordEditable] = useState(false);
 
   const {
-    register: registerPersonalInfo,
-    handleSubmit: handleSubmitPersonalInfo,
-    setValue: setValuePersonalInfo,
-    clearErrors: clearErrorsPersonalInfo,
+    register,
+    handleSubmit,
+    setValue,
+    clearErrors,
     formState: { errors: errorsPersonalInfo },
   } = useForm({
     defaultValues: {
@@ -43,15 +40,15 @@ export default function PersonalInfo() {
 
   const cancelPersonalInfoEdit = () => {
     setEditable(false);
-    setValuePersonalInfo("firstName", currentUser.first_name);
-    setValuePersonalInfo("lastName", currentUser.last_name);
-    setValuePersonalInfo("displayName", currentUser.display_name);
-    setValuePersonalInfo("email", currentUser.email);
+    setValue("firstName", currentUser.first_name);
+    setValue("lastName", currentUser.last_name);
+    setValue("displayName", currentUser.display_name);
+    setValue("email", currentUser.email);
 
-    clearErrorsPersonalInfo("firstName");
-    clearErrorsPersonalInfo("lastName");
-    clearErrorsPersonalInfo("displayName");
-    clearErrorsPersonalInfo("email");
+    clearErrors("firstName");
+    clearErrors("lastName");
+    clearErrors("displayName");
+    clearErrors("email");
   };
 
   const [updateUserMutation] = useMutation(UPDATE_USER_MUTATION);
@@ -60,7 +57,7 @@ export default function PersonalInfo() {
     setLoading(true);
     setPersonalInfoEditSaving(true);
 
-    setTimeout( async() => {
+    setTimeout(async () => {
       try {
         setEditable(false);
         console.log(FormData);
@@ -72,23 +69,32 @@ export default function PersonalInfo() {
               display_name: FormData.displayName,
               email: FormData.email,
               gender: currentUser.gender,
-              user_image: currentUser.image,           
+              user_image: currentUser.image,
             },
           },
         });
-    
+
         // Dispatch the action to update user details in Redux
-        await dispatch(updateUserDetails({ userId: currentUser.id, updates: updatedUser.data.updateUser }));
-    
-        // Set form values after Redux update
-        setValuePersonalInfo("firstName", updatedUser.data.updateUser.first_name);
-        setValuePersonalInfo("lastName", updatedUser.data.updateUser.last_name);
-        setValuePersonalInfo("displayName", updatedUser.data.updateUser.display_name);
-        setValuePersonalInfo("email", updatedUser.data.updateUser.email);
-    
-        toast.success(
-          <ToastComponent message="Successfully updated." />
+        await dispatch(
+          updateUserDetails({
+            userId: currentUser.id,
+            updates: updatedUser.data.updateUser,
+          })
         );
+
+        // Set form values after Redux update
+        setValue("firstName", updatedUser.data.updateUser.first_name);
+        setValue("lastName", updatedUser.data.updateUser.last_name);
+        setValue("displayName", updatedUser.data.updateUser.display_name);
+        setValue("email", updatedUser.data.updateUser.email);
+
+        // clear errors
+        clearErrors("firstName");
+        clearErrors("lastName");
+        clearErrors("displayName");
+        clearErrors("email");
+
+        toast.success(<ToastComponent message="Successfully updated." />);
       } catch (error) {
         console.log(error);
       } finally {
@@ -96,69 +102,6 @@ export default function PersonalInfo() {
         setPersonalInfoEditSaving(false);
       }
     }, 10000);
-    
-  };
-  
-
-  // change password logic
-  const hiddenPasswordPlaceholder = "******";
-
-  const {
-    register: passwordRegister,
-    handleSubmit: passwordHandleSubmit,
-    setValue: passwordSetValue,
-    clearErrors: passwordClearErrors,
-    formState: { errors: errorsPassword },
-  } = useForm({
-    defaultValues: {
-      currentPassword: "",
-      newPassword: "",
-      newPasswordConfirm: "",
-    },
-  });
-
-  const handlePasswordEditEnable = () => {
-    setPasswordEditable(true);
-  };
-
-  const cancelPasswordEdit = () => {
-    passwordSetValue("currentPassword", "");
-    passwordSetValue("newPassword", "");
-    passwordSetValue("newPasswordConfirm", "");
-
-    passwordClearErrors("currentPassword");
-    passwordClearErrors("newPassword");
-    passwordClearErrors("newPasswordConfirm");
-
-    setPasswordEditable(false);
-  };
-
-  const handlePasswordChange = async (FormData) => {
-    setLoading(true);
-    setPasswordChangeSaving(true);
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-
-      passwordSetValue("currentPassword", "");
-      passwordSetValue("newPassword", "");
-      passwordSetValue("newPasswordConfirm", "");
-
-      passwordClearErrors("currentPassword");
-      passwordClearErrors("newPassword");
-      passwordClearErrors("newPasswordConfirm");
-
-      setPasswordEditable(false);
-      console.log("changed password", FormData);
-      toast.success(
-        <ToastComponent message="Successfully changed password." />
-      );
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-      setPasswordChangeSaving(false);
-    }
-    console.log("successfully changed password", FormData);
   };
 
   return (
@@ -181,7 +124,7 @@ export default function PersonalInfo() {
           )}
         </div>
         <Form.Root
-          onSubmit={handleSubmitPersonalInfo(handlePersonalInfoEdit)}
+          onSubmit={handleSubmit(handlePersonalInfoEdit)}
           className="edit-personal-info-form"
         >
           <Form.Field className="edit-personal-info-field">
@@ -190,7 +133,7 @@ export default function PersonalInfo() {
               <input
                 disabled={!editable}
                 type="text"
-                {...registerPersonalInfo("firstName", {
+                {...register("firstName", {
                   required: "This is required",
                 })}
               />
@@ -205,7 +148,7 @@ export default function PersonalInfo() {
               <input
                 disabled={!editable}
                 type="text"
-                {...registerPersonalInfo("lastName", {
+                {...register("lastName", {
                   required: "This is required",
                 })}
               />
@@ -220,7 +163,7 @@ export default function PersonalInfo() {
               <input
                 disabled={!editable}
                 type="text"
-                {...registerPersonalInfo("displayName", {
+                {...register("displayName", {
                   required: "This is required",
                 })}
               />
@@ -235,7 +178,7 @@ export default function PersonalInfo() {
               <input
                 disabled={!editable}
                 type="text"
-                {...registerPersonalInfo("email", {
+                {...register("email", {
                   required: "This is required",
                 })}
               />
@@ -249,100 +192,11 @@ export default function PersonalInfo() {
               {editable && (
                 <PrimaryButton loading={loading} className="save-button">
                   Save
-                  {personalInfoEditSaving? <Spinner /> : null}
+                  {personalInfoEditSaving ? <Spinner /> : null}
                 </PrimaryButton>
               )}
             </Form.Submit>
           </Form.Field>
-        </Form.Root>
-      </div>
-
-      {/* change password form */}
-      <div className="change-password">
-        <div className="change-password-enable">
-          <h3>Set a new password</h3>
-          {!passwordEditable ? (
-            <PrimaryButton
-              action={handlePasswordEditEnable}
-              loading={loading}
-              className="edit-button"
-            >
-              Change
-            </PrimaryButton>
-          ) : (
-            <PrimaryButton action={cancelPasswordEdit} loading={loading}>
-              Cancel
-            </PrimaryButton>
-          )}
-        </div>
-        <Form.Root
-          className="change-password-form"
-          onSubmit={passwordHandleSubmit(handlePasswordChange)}
-        >
-          <Form.Field className="edit-personal-info-field">
-            <Form.Label className="field-label">{passwordEditable ? "Enter your current password" : "Current password"}</Form.Label>
-            <Form.Control className="change-password-field" asChild>
-              <input
-                placeholder={hiddenPasswordPlaceholder}
-                disabled={!passwordEditable}
-                type="password"
-                {...passwordRegister("currentPassword", {
-                  required: "This is required",
-                })}
-              />
-            </Form.Control>
-            <div className="field-message">
-              {errorsPassword.currentPassword?.message}
-            </div>
-          </Form.Field>
-          {passwordEditable && (
-            <>
-              <Form.Field className="edit-personal-info-field">
-                <Form.Label className="field-label">New password</Form.Label>
-                <div className="password-visibility-wrapper">
-                  <Form.Control asChild>
-                    <input
-                      type="password"
-                      {...passwordRegister("newPassword", {
-                        required: "This is required",
-                      })}
-                    />
-                  </Form.Control>
-                  <PasswordVisibilityToggle action={(e) => {}} />
-                </div>
-                <div className="field-message">
-                  {errorsPassword.newPassword?.message}
-                </div>
-              </Form.Field>
-              <Form.Field className="edit-personal-info-field">
-                <Form.Label className="field-label">
-                  Retype new password
-                </Form.Label>
-                <div className="password-visibility-wrapper">
-                  <Form.Control asChild>
-                    <input
-                      type="password"
-                      {...passwordRegister("newPasswordConfirm", {
-                        required: "This is required",
-                      })}
-                    />
-                  </Form.Control>
-                  <PasswordVisibilityToggle action={(e) => {}} />
-                </div>
-                <div className="field-message">
-                  {errorsPassword.newPasswordConfirm?.message}
-                </div>
-              </Form.Field>
-              <Form.Field className="edit-personal-info-field">
-                <Form.Submit asChild>
-                  <PrimaryButton className="save-button" loading={loading}>
-                    Save
-                    {passwordChangeSaving? <Spinner /> : null}
-                  </PrimaryButton>
-                </Form.Submit>
-              </Form.Field>
-            </>
-          )}
         </Form.Root>
       </div>
     </section>
