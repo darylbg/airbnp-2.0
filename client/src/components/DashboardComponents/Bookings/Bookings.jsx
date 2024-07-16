a77c336384cfbb9b3de764c7f4f04858298beb74
Daryl Blough <darylxcuf@gmail.com>
2024-07-16 10:14:59 +0100

    integrated radix ui colors. styled 4 variations of primary button

--- client/src/components/DashboardComponents/Bookings/Bookings.jsx
+++ client/src/components/DashboardComponents/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import DashboardHeader from "../DashboardHeader/DashboardHeader";
 import AddressSearch from "../../AddressSearch/AddressSearch";
+import PrimaryButton from "../../PrimitiveComponents/PrimaryButton/PrimaryButton";
 import "./Bookings.css";
 import DialogComponent from "../../PrimitiveComponents/DialogComponent/DialogComponent";
 
@@ -24,6 +25,33 @@ export default function Bookings() {
         subtitle="Manage your bookings"
         icon="today"
       ></DashboardHeader>
+      <div
+        style={{
+          backgroundColor: "white",
+          display: "flex",
+          gap: "25px",
+          padding: "25px",
+        }}
+      >
+        <PrimaryButton loading={false} className="action-button">action</PrimaryButton>
+        <PrimaryButton loading={false} className="main-button">main</PrimaryButton>
+        <PrimaryButton loading={false} className="control-button">control</PrimaryButton>
+        <PrimaryButton loading={false} className="secondary-button">secondary</PrimaryButton>
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "#e5e5e5",
+          display: "flex",
+          gap: "25px",
+          padding: "25px",
+        }}
+      >
+        <PrimaryButton loading={true} className="action-button">action</PrimaryButton>
+        <PrimaryButton loading={true} className="main-button">main</PrimaryButton>
+        <PrimaryButton loading={true} className="control-button">control</PrimaryButton>
+        <PrimaryButton loading={true} className="secondary-button">secondary</PrimaryButton>
+      </div>
     </>
   );
 }
